3713e4ffac01d7fe1478c24e5d7d76ea2ceba940
Eric Vyncke <eric@vyncke.org>
2022-02-16 23:42:12 +0100

    Sorted analysed AS

--- parse_analysed_as.py
+++ parse_analysed_as.py
@@ -2,8 +2,11 @@
 
 import json
 
+def get_asn(a):
+	return int(a['asn'])
 
 print('|AS Number|AS Description|')
 all_as = json.load(open('analysed_as.json'))
+all_as.sort(key = get_asn)
 for asn in all_as:
 	print('|', asn['asn'], '|', asn['asn_description'], '|')
