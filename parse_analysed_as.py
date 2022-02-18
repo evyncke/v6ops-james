35b3667e49c08f4ea792e079ae44dc23b554f28b
Eric Vyncke <eric@vyncke.org>
2022-02-18 16:02:29 +0100

    Add list of tier-1

--- parse_analysed_as.py
+++ parse_analysed_as.py
@@ -2,11 +2,21 @@
 
 import json
 
+# From https://en.wikipedia.org/wiki/Tier_1_network#cite_note-11
+
+tier1 = {7018, 3320, 3257, 6830, 3356, 2914, 5511, 3491, 1239, 6453, 6762, 1299, 12956, 701, 6461 }
+regional_tier = {4134, 4809, 7473, 174, 7922, 6939, 9002, 1273, 2828, 4637 }
+ 
 def get_asn(a):
 	return int(a['asn'])
 
-print('|AS Number|AS Description|')
+print('|AS Number|AS Description|Comment|')
 all_as = json.load(open('analysed_as.json'))
 all_as.sort(key = get_asn)
 for asn in all_as:
-	print('|', asn['asn'], '|', asn['asn_description'], '|')
+	comment = ''
+	if int(asn['asn']) in tier1:
+		comment = 'Tier-1'
+	elif int(asn['asn']) in regional_tier:
+		comment = 'Regional Tier'
+	print('|', asn['asn'], '|', asn['asn_description'], '|',comment,'|')
